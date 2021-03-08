83b5f380c2ddf874322a2e8d09aaddd8c2c892f7
Liam Björkman <liambjorkman00@gmail.com>
2021-03-08 11:00:46 +0100

    Update IBeverage.cs
    Attributes

--- BaristaAPI/IBeverage.cs
+++ BaristaAPI/IBeverage.cs
@@ -5,8 +5,11 @@ namespace The_barista
     public interface IBeverage
     {
         public string Name { get; }
+        [Description("The ingredients for the current coffee object")]
         List<string> Ingredients { get; }
+        [Description("Sets the coffee cup size")]
         CupType CupType { get; set; }
+        [Description("Sets the coffee temperature")]
         public int Degree { get; set; }
     }
 }
